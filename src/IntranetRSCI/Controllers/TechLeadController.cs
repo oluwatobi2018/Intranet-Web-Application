47f4b4bd231396e0f96928144871a9899e3ff082
cmetzger4 <cmetzger4@gmail.com>
2016-08-15 14:43:18 -0400

    Create Tech Lead Form Controller and View

---
+++ src/IntranetRSCI/Controllers/TechLeadController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IntranetRSCI.Controllers
+{
+    public class TechLeadController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
