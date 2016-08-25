28dd3ce5d76be6acf47952b80f50a9658a54ddf1
cmetzger4 <cmetzger4@gmail.com>
2016-08-25 11:12:45 -0400

    Issue #2 - Banner Links Fixed Removed the invalid banner links and redirected the "Home" link to the RaymondSCI webpage.

--- src/IntranetRSCI/Controllers/HomeController.cs
+++ src/IntranetRSCI/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace IntranetRSCI.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return Redirect("http://raymondsci.com/");
         }
 
         public IActionResult About()
